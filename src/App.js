c70f0a542cf1e89e59ef284a72f78af22d67bebf
tkoar <terrancekoar@gmail.com>
2017-07-17 16:27:45 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App component
-      </div>
-    );
-  }
+
+ handleOnClick() {
+   this.props.store.dispatch({
+     type: 'INCREASE_COUNT',
+   });
+ }
+
+ render() {
+   return (
+     <div className="App">
+       <button onClick={(event) => this.handleOnClick(event)} >
+         Click
+       </button>
+       <p>{this.props.store.getState().items.length}</p>
+     </div>
+   );
+ }
 };
+
 
 export default App;
--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,12 +1,17 @@
+
 export default function shoppingListItemReducer(state = {
-  items: []
+ items: []
 }, action) {
-  switch(action.type) {
+ console.log(action);
+ switch(action.type) {
 
-    case 'INCREASE_COUNT':
-      return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
+   case 'INCREASE_COUNT':
+     console.log("Current state.items length %s", state.items.length);
+     console.log("Updating state.items length to %s", state.items.length + 1);
+     return Object.assign({}, state, { items: state.items.concat(state.items.length + 1) });
 
-    default:
-      return state;
-  }
+   default:
+     console.log("Initial state.items length: %s", state.items.length);
+     return state;
+ }
 }
